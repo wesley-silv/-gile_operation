3f916e2d52385e89eee782eb4e827defbd0aa91d
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2022-11-11 22:39:48 -0300

    acrescimo do iframe.

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 function Main () {
   return (
+    <>
     <div className='main'>
       <img src="../imagens/Manutenção.jpg.jpg" alt="Imagem de um homem tocando engrenagens fluetuantes " />
       <div className='main-title'>
@@ -15,6 +16,13 @@ function Main () {
       </p>
       </div>
     </div>
+    <div className='video'> 
+    <h2>
+      Assista o vídeo para saber mais
+    </h2>
+    <iframe className='iframe' src="https://www.youtube.com/embed/29NRtD0RAKM" width="560" height="315" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
+    </div>
+    </>
     
   )
 }
